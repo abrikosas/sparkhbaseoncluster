0855809c5ce70b30bdedde326d3f05eed4c3b428
abrikosas <algirdas.klimavicius@gmail.com>
2017-03-19 20:00:28 +0100

    all hardcoded values added as parameters

--- src/main/scala/HBaseStream.scala
+++ src/main/scala/HBaseStream.scala
@@ -11,21 +11,34 @@ import org.apache.spark.SparkContext
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-
+/**
+  * * @param args(0)        - port
+  * * @param args(1)        - host
+  * * @param args(2)        - hbase table name
+  * * @param args(3)        - column family name
+  * * @param args(4)        - window size
+  * * @param args(5)        - mapreduce output
+  */
 
 object HBaseAttackStream extends Serializable {
-  final val tableName = "test:attacksv3"
-  final val cfAttacker = Bytes.toBytes("af")
+  //final val cfAttacker = Bytes.toBytes("af")
   //final val patternList = List("Failed","failed")
   final val patternList = List("Invalid")
+  //  final val tableName = "test:attacksv3"
 
   def main(args: Array[String]): Unit = {
     // set up HBase Table configuration
+    val tableName = args(2)
+    val cfAttacker = args(3)
+    val host = args(1)
+    val port = args(0).toInt
+    val windowSize = args(4)
+    val mrOutput = args(5)
     val conf = HBaseConfiguration.create()
     conf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
 
     val jobConfig: JobConf = new JobConf(conf, this.getClass)
-jobConfig.set("mapreduce.output.fileoutputformat.outputdir", "/tmp/out")
+    jobConfig.set("mapreduce.output.fileoutputformat.outputdir", mrOutput)
     jobConfig.setOutputFormat(classOf[TableOutputFormat])
     jobConfig.set(TableOutputFormat.OUTPUT_TABLE, tableName)
 
@@ -33,25 +46,18 @@ jobConfig.set("mapreduce.output.fileoutputformat.outputdir", "/tmp/out")
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
     val sc = new SparkContext()
-    //    val spark = SparkSession
-    //      .builder
-    //      .appName(getClass.getSimpleName)
-    //      .getOrCreate()
-    //val sc = spark.sparkContext
-    val ssc = new StreamingContext(sc, Seconds(1))
+    val ssc = new StreamingContext(sc, Seconds(windowSize.toInt))
     println("Stream processing logic start")
-    val attackDStream = ssc.socketTextStream("localhost", args(0).toInt, StorageLevel.MEMORY_AND_DISK_SER).map(Attack.parseEvent)
+    val attackDStream = ssc.socketTextStream(host, port, StorageLevel.MEMORY_AND_DISK_SER).map(Attack.parseEvent)
     attackDStream.print()
 
     attackDStream.foreachRDD { rdd =>
 
       // convert Attack data to put object and write to HBase table column family data
-      rdd.map(Attack.convertToPut).saveAsHadoopDataset(jobConfig)
+      rdd.map(Attack.convertToPut(_, cfAttacker)).saveAsHadoopDataset(jobConfig)
     }
 
-    // Start the computation
     ssc.start()
-    // Wait for the computation to terminate
     ssc.awaitTermination()
 
   }
@@ -73,13 +79,13 @@ jobConfig.set("mapreduce.output.fileoutputformat.outputdir", "/tmp/out")
     }
 
     //  Convert a row of Attack object data to an HBase put object
-    def convertToPut(attack: ShortAttack): (ImmutableBytesWritable, Put) = {
+    def convertToPut(attack: ShortAttack, cfAttacker: String): (ImmutableBytesWritable, Put) = {
       val dateTime = attack.timestamp
       // create a composite row key: Attackid_date time
       val rowkey = attack.ip
       val put = new Put(Bytes.toBytes(rowkey))
       // add to column family data, column  data values to put object
-      put.add(cfAttacker, Bytes.toBytes(dateTime), Bytes.toBytes(attack.realuser))
+      put.add(Bytes.toBytes(cfAttacker), Bytes.toBytes(dateTime), Bytes.toBytes(attack.realuser))
       return (new ImmutableBytesWritable(Bytes.toBytes(rowkey)), put)
     }
 
